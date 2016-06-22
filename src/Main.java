d42f1193ce34b7225637b17de5983ebcdda07af0
Dylan <dylanjones2011@gmail.com>
2016-06-22 12:22:18 -0400

    Made Main more visible

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 
+
+import javax.swing.JFrame;
+
+import world.DiepPanel;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Diep.io");
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new DiepPanel());
+		frame.setVisible(true);
 
 	}
 
